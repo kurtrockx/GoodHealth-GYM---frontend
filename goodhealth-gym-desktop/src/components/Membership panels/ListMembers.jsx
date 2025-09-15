0f5588ab9b2f545351f2e8510bae393722edfa79
derpfear <kurtdebelen431@gmail.com>
2025-09-16 06:07:47 +0800

    changed the sample data to be at least accurate to the production data

--- goodhealth-gym-desktop/src/components/Membership panels/ListMembers.jsx
+++ goodhealth-gym-desktop/src/components/Membership panels/ListMembers.jsx
@@ -2,41 +2,41 @@ import editIcon from "../../assets/editIcon.png";
 import addMembersIcon from "../../assets/addMembersIcon.png";
 
 const members = [
-  {
-    name: "John Doe",
-    type: "Regular",
-    membership: "Gold",
-    contactNumber: "123-456-7890",
-    email: "johndoe@example.com",
-  },
-  {
-    name: "Jane Smith",
-    type: "Premium",
-    membership: "Platinum",
-    contactNumber: "987-654-3210",
-    email: "janesmith@example.com",
-  },
-  {
-    name: "Alice Johnson",
-    type: "Regular",
-    membership: "Silver",
-    contactNumber: "456-789-0123",
-    email: "alicejohnson@example.com",
-  },
-  {
-    name: "Bob Brown",
-    type: "Guest",
-    membership: "Day Pass",
-    contactNumber: "321-654-0987",
-    email: "bobbrown@example.com",
-  },
-  {
-    name: "Charlie Davis",
-    type: "Regular",
-    membership: "Gold",
-    contactNumber: "789-012-3456",
-    email: "charliedavis@example.com",
-  },
+    {
+        name: "John Doe",
+        type: "Student",
+        membership: "4 1/2 months",
+        contactNumber: "123-456-7890",
+        email: "johndoe@example.com",
+    },
+    {
+        name: "Jane Smith",
+        type: "Student",
+        membership: "4 1/2 months",
+        contactNumber: "987-654-3210",
+        email: "janesmith@example.com",
+    },
+    {
+        name: "Alice Johnson",
+        type: "Student",
+        membership: "4 1/2 months",
+        contactNumber: "456-789-0123",
+        email: "alicejohnson@example.com",
+    },
+    {
+        name: "Bob Brown",
+        type: "Student",
+        membership: "4 1/2 months",
+        contactNumber: "321-654-0987",
+        email: "bobbrown@example.com",
+    },
+    {
+        name: "Charlie Davis",
+        type: "Student",
+        membership: "4 1/2 months",
+        contactNumber: "789-012-3456",
+        email: "charliedavis@example.com",
+    },
 ];
 
 function Categories() {
